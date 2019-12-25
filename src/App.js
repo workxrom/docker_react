d667fa15c9f3c98ced5eab4007cb54ed9165284e
Nikita <xpom5566@mail.ru>
2019-12-25 12:49:23 +0300

    feat: change text

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello NATALIA !!! GLAD TO HEAR YOU HERE !!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 222 333
+          Hello !!!
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -2,15 +2,16 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+const text = "Hello !!!";
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(text);
   expect(linkElement).toBeInTheDocument();
 });
 
 test("renders learn react link 2", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(text);
   expect(linkElement).toBeInTheDocument();
 });
 
